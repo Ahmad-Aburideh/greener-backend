5f127863c3a7a8d72f0654030ecf50f4432745ee
ahmad <ahmad.abureidah@gmail.com>
2026-04-14 20:25:50 +0300

    remove jwt filter test

--- greener/src/main/java/com/greener/auth/security/SecurityConfig.java
+++ greener/src/main/java/com/greener/auth/security/SecurityConfig.java
@@ -24,8 +24,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                );
 
         return http.build();
     }
